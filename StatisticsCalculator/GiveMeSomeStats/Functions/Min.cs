1d3140f5fc3a787ce37ee2437506e1ef26251434
bigbyj <shawn.jm@hotmail.com>
2019-10-13 23:01:01 +0200

    intial check in

---
+++ StatisticsCalculator/GiveMeSomeStats/Functions/Min.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using GiveMeSomeStats.Interface;
+
+namespace GiveMeSomeStats.Functions
+{
+    public class Min : ICalculate
+    {
+        public double Calculate(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Calculate(List<int> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StatisticsCalculator/GiveMeSomeStats/Interface/ICalculate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GiveMeSomeStats.Interface
+{
+    public interface ICalculate
+    {
+        double Calculate(int x, int y);
+        double Calculate(List<int> values);
+    }
+}
